9de0ae41346471385c7d66e31d1e6c2380ad5071
Whirligig231 <gigilrihw@gmail.com>
2024-02-10 17:22:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Kalamagic
+
+## A FOSS PyAudio-based voice changer
+
+Kalamagic is a realtime voice changer written in Python using the PyAudio library. It does have some other dependencies that are unfortunately pretty heavy (NumPy and SciPy, notably), but mostly they're dependencies that I figure the kind of person who tinkers with Python a lot will already have, and if not, they're easy to install.
+
+The purpose of this project was that I've seen a lot of "free voice changer for streamers" software out there, and most of it either uses AI, is "free" as in "you need to pay us to use 90% of this program but we'll let you use like 2 features for free," or both. My goal here was to create something simple, configurable, and AI-free.
+
+The name is derived from the toki pona word "kalama," meaning "sound," and the English word "magic."
+
+### Quickstart Guide
+
+If you just want to quickly get your voice changed, here's how:
+
+1. Install [Python 3](https://www.python.org/) if you don't already have it. (Sorry, I have no idea what the minimum version is, but anything vaguely up-to-date should work.)
+2. Opening a command line, run the following commands:
+	- `python -m pip install numpy`
+	- `python -m pip install scipy`
+	- `python -m pip install pyaudio`
+	- `python -m pip install keyboard`
+	The first two of these will probably take the longest (if you do not already have them installed).
+3. Clone this repository. There's no need to build anything; it's Python.
+4. Opening a command line, navigate to the repository directory and run `python kalamagic.py`.
+5. You will first be prompted to select an input device from a list. This should be your microphone or whatever you want to use to feed audio to the program. When in doubt, open another audio program (e.g. Discord) and check what it's set to use.
+	(NOTE: you'll probably see each device appear several times with different prefixes; for example, my microphone shows up once as MME, once as Windows DirectSound, once as Windows WASAPI, and once as Windows WDM-KS. These represent different interfaces to access the same device; ideally they will all work, but in practice you may want to try different ones to see if the performance is better or worse.)
+6. You will next be prompted to select an output device from a list. For now, select your speakers/headphones.
+7. Lastly, you will be asked to choose a filter from a list. Pick whichever one you like (I recommend blackhelmet.txt).
+8. You should be able to hear yourself now!
+
+### How do I use it for Discord, OBS, etc.?
+
+Kalamagic can only output to an audio *output* device, so you need some way to feed that output into an *input* device. There are many ways to do this, from advanced software packages to the old "plug the line out into the line in" trick. One easy way on Windows is [VB-Audio Virtual Cable](https://vb-audio.com/Cable/).
+
+### Configuration
+
+The config.json file contains five fields that control how Kalamagic runs:
+
+* `inputDevice`: This is the name of the audio input device. If this device is not found, you will be prompted to select one, and the configuration file will be updated. So to change your device, simply replace the name with an empty string.
+* `outputDevice`: This is the name of the audio output device. As with the input device, you will be prompted if this does not exist, and the configuration file will be updated.
+* `bufferLength`: This is how long the *buffer* is, i.e. how much audio the program will work with at a time. It takes time to do the audio computations, so if this length is too low, you will see `! BUFFER OVERRUN !` in the console, and the audio will sound distorted. If this happens, increase the setting and restart the program.
+* `historyLength`: This is how much previous audio Kalamagic keeps track of. If this value is too short, some audio effects will sound distorted, but the program's RAM usage will be proportional to this value.
+* `sampleRate`: This is the sample rate used by the program. You should probably set this to be equal to the sample rate of your microphone, but you can lower it if your CPU isn't fast enough for a given filter.
+
+### Filter Files
+
+Each filter file contains one or more *blocks,* which are individual filters applied to an audio track. A block takes some number of tracks as input and produces one track of output.
+
+A *track* is one complete track of audio (two channels, left and right). Tracks are numbered starting from 0; track 0 is the input, and track 1 is the output. A track should only be used as the output of one block (except for track 0, which should not be used as the output of any block), and tracks should be numbered sequentially (if a filter file uses a track, the program will assume it will also use every lower-numbered track). The more tracks a file uses, the higher the RAM consumption and computational load of the filter.
+
+Below is a list of every possible block:
+
+* `[a] -> [q] identity`: The identity block; this copies track `a` to track `q` without alteration. This is mostly useful for debugging.
+* `[a] -> [q] lowpass [hz]`: This simple low-pass filter sets `q` to be `a` with a low-pass centered on the frequency of `hz` hertz.
+* `[a] -> [q] basichighpass`: This is a very basic high-pass filter. I'm not experienced enough in digital audio processing algorithms to know how to add a frequency setting to this.
+* `[a] -> [q] amplify [db]`: Amplifies track `a` by `db` decibels to produce track `q`. This can be positive or negative.
+* `[a] -> [q] tanhlimit`: This applies the hyperbolic tangent (tanh) function to track `a` to produce track `q`, for the purpose of preventing clipping while preserving dynamic range.
+* `[a] -> [q] delay [ms] [t]`: This applies a delay effect to track `a`, with a delay of `ms` milliseconds and a feedback factor of `t`. So if `t` is set to `0.3`, for instance, then each echo will be `0.3` times as loud as the previous one.
+* `[a] -> [q] delayinvert [ms] [t]`: This is the same delay effect, but this one also switches the stereo channels in the signal with every delay. This is helpful for reverb filters, for instance.
+* `[a] [b] -> [q] [t]`: This mixes `a` and `b` to create `q`. The parameter `t` controls the mix; when `t = 0`, `q` will copy `a`, and when `t = 1`, `q` will copy `b`.
+* `[a] -> [q] pitchshift [pitch] [cutoff]`: This applies a basic pitch-shifting effect to `a` to produce `q`. Here, `pitch` is a multiplier; i.e. the pitch frequency of `q` will be the pitch frequency of `a` times `pitch`. So if you want to pitch-shift by `n` cents, you should find the value of 2^(`n`/1200) and use that as your `pitch` value. (Keep in mind that this effect is probably too rough to be usable for precise musical filters.) The value `cutoff` is a frequency in hertz controlling the length of the window used by the block; frequencies below the value will not be pitch-shifted, but the lower the value is, the more latency will be introduced.
+* `[a] -> [q] pitchshift [pitch]`: This is the same as above, but it uses the default `cutoff` value of 172.
+* `[a] -> [q] rectify`: This takes the absolute value of the signal `[a]`. This is used to create an octave-up effect by some guitar pedals.
+* `[q] sine [hz]`: This generates a constant sine wave of `hz` hertz.
+* `[q] saw [hz]`: This generates a constant sawtooth wave of `hz` hertz.
+* `[q] square [hz]`: This generates a constant square wave of `hz` hertz.
+* `[q] square [hz] [duty]`: This generates a constant pulse wave of `hz` hertz, with the duty cycle set to `duty`. So a value of 0.25 for `duty` will create a 25% pulse.
+* `[q] triangle [hz]`: This generates a constant triangle wave of `hz` hertz.
+* `[a] [b] -> [q] mult`: This multiplies the signals `a` and `b` together. This can be used for some modulation effects.
+* `[q] keypress [key]`: This generates a signal that is `1` or `-1` depending on whether or not the key named by `key` is pressed.
+* `[a] [b] [c] -> [q] modmix`: This is effectively the `mix` block, but with the mix factor determined by a third track. When `c` is `-1`, `q` will be `a`, and when `c` is `1`, `q` will be `b`.
+* `[q] wavfile [fname]`: Provide in `fname` the path to a signed 16-bit stereo WAV file from where the program is run. Then, `q` will be a track that loops the WAV file constantly. This does not resample intelligently (it uses nearest-neighbor), so for best results, use a file with the same sample rate as the one in config.json.
---
+++ kalamagic.py
@@ -0,0 +1,394 @@
+import pyaudio
+from scipy.io import wavfile
+import numpy as np
+import time
+import json
+import sys
+import os
+import keyboard
+
+print('Loading configuration ...')
+
+config = None
+if not os.path.isfile('config.json'):
+    fp = open('defaults.json', 'r')
+    config = json.load(fp)
+    fp.close()
+else:
+    fp = open('config.json', 'r')
+    config = json.load(fp)
+    fp.close()
+
+BUFFER = config['bufferLength']
+MAXBUFF = config['historyLength']
+RATE = config['sampleRate']
+
+terminate = False
+
+print('Loading devices ...')
+
+p=pyaudio.PyAudio()
+numdevices = p.get_device_count()
+
+def getDeviceFullName(i):
+    return p.get_host_api_info_by_index(p.get_device_info_by_index(i).get('hostApi')).get('name') + ' - ' + p.get_device_info_by_index(i).get('name')
+
+choice = 'blah'
+
+for i in range(0, numdevices):
+    if (p.get_device_info_by_index(i).get('maxInputChannels')) > 0:
+        if getDeviceFullName(i) == config['inputDevice']:
+            choice = str(i+1)
+
+if not choice.isdigit():
+    print('')
+    print('Input device "' + config['inputDevice'] + '" not found, please select one:')
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_index(i).get('maxInputChannels')) > 0:
+            print(str(i+1) + ".", getDeviceFullName(i))
+            
+    print('')
+    while not choice.isdigit() or int(choice) <= 0 or int(choice) > numdevices:
+        choice = input('Enter an audio device number or 0 to exit: ')
+        if choice == '0':
+            terminate = True
+            break
+    print('')
+
+if terminate:
+    sys.exit(0)
+
+choice2 = 'blah'
+
+for i in range(0, numdevices):
+    if (p.get_device_info_by_index(i).get('maxOutputChannels')) > 0:
+        if getDeviceFullName(i) == config['outputDevice']:
+            choice2 = str(i+1)
+
+if not choice2.isdigit():
+    print('')
+    print('Output device "' + config['outputDevice'] + '" not found, please select one:')
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_index(i).get('maxOutputChannels')) > 0:
+            print(str(i+1) + ".", getDeviceFullName(i))
+            
+    print('')
+    while not choice2.isdigit() or int(choice2) <= 0 or int(choice2) > numdevices:
+        choice2 = input('Enter an audio device number or 0 to exit: ')
+        if choice2 == '0':
+            terminate = True
+            break
+    print('')
+
+if terminate:
+    sys.exit(0)
+
+config['inputDevice'] = getDeviceFullName(int(choice)-1)
+config['outputDevice'] = getDeviceFullName(int(choice2)-1)
+
+print('Saving configuration ...')
+
+fp = open('config.json', 'w')
+json.dump(config, fp, indent=4)
+fp.close()
+
+print('')
+print('Choose a filter file:')
+
+filelist = os.listdir('filters')
+for i in range(len(filelist)):
+    print(str(i+1) + ".", filelist[i])
+
+choice3 = 'blah'
+print('')
+while not choice3.isdigit() or int(choice3) <= 0 or int(choice3) > len(filelist):
+    choice3 = input('Enter a filter file number or 0 to exit: ')
+    if choice3 == '0':
+        terminate = True
+        break
+print('')
+
+if terminate:
+    sys.exit(0)
+
+print('Loading filter file ...')
+
+chain = []
+maxchannel = 1
+
+wavedata = {}
+
+fp = open('filters/' + filelist[int(choice3)-1], 'r')
+for line in fp:
+    words = line.strip().split(' ')
+    fname = ''
+    args = []
+    starti = 0
+    if len(words) > 1 and words[1] == '->':
+        fname = words[3]
+        args += [int(words[0]), int(words[2])]
+        maxchannel = max(maxchannel, int(words[0]), int(words[2]))
+        starti = 4
+    elif len(words) > 2 and words[2] == '->':
+        fname = words[4]
+        args += [int(words[0]), int(words[1]), int(words[3])]
+        maxchannel = max(maxchannel, int(words[0]), int(words[1]), int(words[3]))
+        starti = 5
+    elif len(words) > 3 and words[3] == '->':
+        fname = words[5]
+        args += [int(words[0]), int(words[1]), int(words[2]), int(words[4])]
+        maxchannel = max(maxchannel, int(words[0]), int(words[1]), int(words[2]), int(words[4]))
+        starti = 6
+    else:
+        fname = words[1]
+        args += [int(words[0])]
+        maxchannel = max(maxchannel, int(words[0]))
+        starti = 2
+    for i in range(starti, len(words)):
+        if words[i].endswith('.wav'):
+            wrate, wdata = wavfile.read(words[i])
+            wavedata[words[i]] = (wrate, wdata)
+        args += [words[i]]
+    chain += [(fname, args)]
+fp.close()
+
+print('File loaded, ' + str(maxchannel+1) + ' tracks required')
+
+TRACKS = maxchannel + 1
+
+idata = [0]*(BUFFER*2+MAXBUFF)
+odata = [0]*MAXBUFF
+
+datas = [idata, odata]
+for i in range(TRACKS-2): # Number of buffers
+    datas += [[0]*MAXBUFF]
+
+iplayhead = BUFFER + MAXBUFF // 2 # Initial buffer
+oplayhead = MAXBUFF//2
+
+framei = 0
+totaltime = 0
+
+def identity(a,b):
+    global datas, framei
+    i = framei
+    datas[b][i] = datas[a][i]
+    datas[b][i+1] = datas[a][i+1]
+
+def lowpass(a,b,hz):
+    global datas, framei
+    i = framei
+    t = np.tan(np.pi * float(hz) / RATE)
+    datas[b][i] = (t * datas[a][i] + t * datas[a][i-2] + (1-t) * datas[b][i-2]) / (t+1)
+    datas[b][i+1] = (t * datas[a][i+1] + t * datas[a][i-1] + (1-t) * datas[b][i-1]) / (t+1)
+
+def basichighpass(a,b):
+    global datas, framei
+    i = framei
+    datas[b][i] = (datas[a][i] - datas[a][i-2]) / (2)
+    datas[b][i+1] = (datas[a][i+1] - datas[a][i-1]) / (2)
+
+def amplify(a,b,db):
+    global datas, framei
+    i = framei
+    fac = np.power(10,float(db)/10)
+    datas[b][i] = datas[a][i] * fac
+    datas[b][i+1] = datas[a][i+1] * fac
+
+def tanhlimit(a,b):
+    global datas, framei
+    i = framei
+    datas[b][i] = np.tanh(datas[a][i])
+    datas[b][i+1] = np.tanh(datas[a][i+1])
+
+def delay(a,b,ms,t):
+    global datas, framei
+    i = framei
+    d = int((float(ms) * RATE) // 1000)
+    t = float(t)
+    datas[b][i] = datas[a][i] * (1-t) + datas[b][i-2*d] * t
+    datas[b][i+1] = datas[a][i+1] * (1-t) + datas[b][i+1-2*d] * t
+
+def delayinvert(a,b,ms,t):
+    global datas, framei
+    i = framei
+    d = int((float(ms) * RATE) // 1000)
+    t = float(t)
+    datas[b][i] = datas[a][i] * (1-t) + datas[b][i+1-2*d] * t
+    datas[b][i+1] = datas[a][i+1] * (1-t) + datas[b][i-2*d] * t
+    
+def mix(a,b,c,t):
+    global datas, framei
+    i = framei
+    t = float(t)
+    datas[c][i] = datas[a][i] * (1-t) + datas[b][i] * t
+    datas[c][i+1] = datas[a][i+1] * (1-t) + datas[b][i+1] * t
+    
+def pitchshift(a,b,pitch,cutoff=172):
+    global datas, framei
+    i = framei
+    
+    pitch = float(pitch)
+    cutoff = float(cutoff)
+    
+    cutoffLen = int(RATE / cutoff / 2) * 2
+    
+    imod = i % cutoffLen
+    chunki = i - imod
+    pchunki = chunki - cutoffLen
+
+    pcind1 = int(imod * pitch / 2) * 2
+    pcind2 = int((imod * pitch + (1 - pitch) * cutoffLen) / 2) * 2
+    
+    t = imod / cutoffLen
+    
+    datas[b][i] = datas[a][pchunki + pcind1] * (1-t) + datas[a][pchunki + pcind2] * t
+    datas[b][i+1] = datas[a][pchunki + pcind1 + 1] * (1-t) + datas[a][pchunki + pcind2 + 1] * t
+
+def rectify(a,b):
+    global datas, framei
+    i = framei
+    datas[b][i] = abs(datas[a][i])
+    datas[b][i+1] = abs(datas[a][i+1])
+
+def sine(a, hz):
+    global datas, framei, totaltime
+    i = framei
+    
+    hz = float(hz)
+    
+    datas[a][i] = np.sin(totaltime*hz*2*np.pi)
+    datas[a][i+1] = np.sin(totaltime*hz*2*np.pi)
+
+def saw(a, hz):
+    global datas, framei, totaltime
+    i = framei
+    
+    hz = float(hz)
+    
+    datas[a][i] = -1 + 2*hz*(totaltime % (1/hz))
+    datas[a][i+1] = -1 + 2*hz*(totaltime % (1/hz))
+
+def square(a, hz, duty=0.5):
+    global datas, framei, totaltime
+    i = framei
+    
+    hz = float(hz)
+    duty = float(duty)
+    
+    p = 1
+    if hz*(totaltime % (1/hz)) > duty:
+        p = -1
+    
+    datas[a][i] = p
+    datas[a][i+1] = p
+
+def triangle(a, hz):
+    global datas, framei, totaltime
+    i = framei
+    
+    hz = float(hz)
+    
+    datas[a][i] = abs((totaltime % (1/hz)) - 1/(2*hz)) * hz - 1
+    datas[a][i+1] = abs((totaltime % (1/hz)) - 1/(2*hz)) * hz - 1
+
+def mult(a,b,c):
+    global datas, framei
+    i = framei
+    datas[c][i] = datas[a][i] * datas[b][i]
+    datas[c][i+1] = datas[a][i+1] * datas[b][i+1]
+
+def keypress(a, key):
+    global datas, framei
+    i = framei
+    
+    if i % 1024 == 0:
+        datas[a][i] = 2*int(keyboard.is_pressed(key))-1
+        datas[a][i+1] = 2*int(keyboard.is_pressed(key))-1
+    else:
+        datas[a][i] = datas[a][i-2]
+        datas[a][i+1] = datas[a][i-1]
+
+def modmix(a,b,c,d):
+    global datas, framei
+    i = framei
+    datas[d][i] = datas[a][i] * (0.5 - 0.5*datas[c][i]) + datas[b][i] * (0.5 + 0.5*datas[c][i])
+    datas[d][i+1] = datas[a][i+1] * (0.5 - 0.5*datas[c][i+1]) + datas[b][i+1] * (0.5 + 0.5*datas[c][i+1])
+
+def wavfile(a, fname):
+    global datas, framei, totaltime, wavedata
+    i = framei
+    
+    wrate = wavedata[fname][0]
+    wdata = wavedata[fname][1]
+    sampleno = round(totaltime * wrate)
+    totalsamps = len(wdata)
+    sampleno = sampleno % totalsamps
+    l = wdata[sampleno][0]
+    r = wdata[sampleno][1]
+    
+    datas[a][i] = l / 32768
+    datas[a][i+1] = r / 32768
+
+print('Initializing engine ...')
+
+def compute_frame():
+    global idata, odata, framei, iplayhead, oplayhead, chain, totaltime
+
+    while len(idata) <= len(odata):
+        print('! BUFFER OVERRUN !')
+        idata += [0]
+    
+    if len(odata) > MAXBUFF * 2:
+        toRemove = len(odata) - MAXBUFF
+        for j in range(TRACKS):
+            del datas[j][:toRemove]
+        iplayhead -= toRemove // 2
+        oplayhead -= toRemove // 2
+       
+    framei = len(odata)
+    odata += [0,0]
+    for j in range(2,TRACKS):
+        datas[j] += [0,0]
+
+    # Compute output buffer
+    for block in chain: # lol
+        globals()[block[0]](*block[1])
+
+    # Clipping
+    odata[framei] = np.clip(odata[framei], -1, 1)
+    odata[framei+1] = np.clip(odata[framei+1], -1, 1)
+    
+    totaltime += 1/RATE
+
+def icallback(in_data, frame_count, time_info, status):
+    global idata,iplayhead
+    while len(idata) < 2*iplayhead+2*frame_count:
+        idata += [0,0]
+    
+    samples = [[x / 16384, x / 16384] for x in np.frombuffer(in_data, dtype=np.int16)]
+    samples = list(np.array(samples).flatten())
+    idata[2*iplayhead:2*iplayhead+2*frame_count] = samples
+    iplayhead += frame_count
+    
+    return(None, pyaudio.paContinue)
+
+def ocallback(in_data, frame_count, time_info, status):
+    global odata,oplayhead
+    while len(odata) < 2*oplayhead+2*frame_count:
+        compute_frame()
+    samples = odata[2*oplayhead:2*oplayhead+2*frame_count]
+    oplayhead += frame_count
+    samplebytes = np.array([s*16384 for s in samples], dtype=np.int16).tobytes()
+    return (samplebytes, pyaudio.paContinue)
+
+ostream = p.open(format=pyaudio.paInt16,channels=2,rate=RATE,output=True,stream_callback=ocallback,output_device_index=int(choice2)-1)
+istream = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,stream_callback=icallback,input_device_index=int(choice)-1)
+
+print('Successfully initialized!')
+
+input()
+
+ostream.close()
+istream.close()
+p.terminate()
